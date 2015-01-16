80c635848ed656aa947f1654eba5135ae603ebad
Yan Xu <xuyantc@msn.com>
2015-01-16 11:24:14 -0500

    initial commit

---
+++ TestFile.java
@@ -0,0 +1,31 @@
+public class TestFile {
+
+	public int i;
+
+	public aMethod (){
+		if (0==0){
+			int i;
+			String a;
+		};
+
+		while(0!=1){
+			break;
+		};
+	}
+
+	public anotherMethod (){
+		if (0==0){
+			int i;
+			String a;
+		};
+
+		if (0==0){
+			int i;
+			String a;
+		};
+
+		while(0!=1){
+			break;
+		};
+	}
+}
